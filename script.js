77619b4a9982603c29ebca1dd166ded6ae7a8fc3
harshalol-ig <harshalolpro@gmail.com>
2025-12-26 20:53:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToSecret() {
+    document.getElementById("step1").style.display = "none";
+    document.getElementById("step2").style.display = "block";
+}
+
+function unlock() {
+    document.getElementById("secretMessage").style.display = "block";
+}
+Script
